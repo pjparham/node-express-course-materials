ec834d3e962033c4104945fe1030a22e7caf90bf
Parker Parham <parkerparham@gmail.com>
2023-03-21 21:16:26 -0400

    adds notes on event emitters

---
+++ 14-emitters.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter()
+
+//on - listen for an event && emit - emits event
+customEmitter.on('response', (name, id) => {
+    console.log(`data recieved user ${name} with id: ${id}`);
+})
+
+customEmitter.on('response', () => {
+    console.log(`some other logic here`);
+})
+
+customEmitter.emit('response', 'john', 34)
